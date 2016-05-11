4e66ec11dd0ccc2d3c9ab02b1805668dbedf7733
Robert Story <rstory@localhost>
2016-05-11 09:47:56 -0400

    fix puppet_cleanup script

--- puppet/modules/rpki/files/puppet_cleanup.sh
+++ puppet/modules/rpki/files/puppet_cleanup.sh
@@ -24,14 +24,22 @@ STATE=/var/lib/puppet/state/state.yaml
 
 # If there is a stale lock file OR the state file hasn't changed in
 # 2 hours time, please kick puppet in the teeth and make it restart.
-FIRST_CANDIDATE=$(find ${LOCK} -cmin +120)
-SECOND_CANDIDATE=$(find ${STATE} -cmin +120)
+if [ -e ${LOCK} ]; then
+    FIRST_CANDIDATE=$(find ${LOCK} -cmin +120)
+fi
+if [ -e ${STATE} ]; then
+    SECOND_CANDIDATE=$(find ${STATE} -cmin +120)
+fi
 
-if [ ! -z ${CANDIDATE} ]; then
-  /bin/rm -f ${CANDIDATE}
+RESTART=0
+for CANDIDATE in $FIRST_CANDIDATE $SECOND_CANDIDATE
+do
+    RESTART=1
+    /bin/rm -f "${CANDIDATE}"
+done
+
+if [ $RESTART -eq 1 ]; then
   /etc/init.d/puppet stop
   /usr/bin/puppet --onetime --no-daemonize > /tmp/puppet.log 2>&1
   /etc/init.d/puppet start
 fi
-
-
