05000fda2c615c7600f4e6fe6338175ab585e5c9
Chris Morrow <morrowc@ops-netman.net>
2016-06-10 12:22:19 -0600

    Make the puppet module install be single-module-at-a-time.
    The puppet module install fails with:
    $ sudo puppet module install puppetlabs-stdlib puppetlabs-inifile
    Error: puppet module install takes 1 argument, but you gave 2
    Error: Try 'puppet help module install' for usage
    
    currently. This fix:
    $ for d in puppetlabs-stdlib puppetlabs-inifile; do
    > sudo puppet module install $d
    > done
    Notice: Preparing to install into /etc/puppet/modules ...
    Notice: Downloading from https://forgeapi.puppetlabs.com ...
    Notice: Installing -- do not interrupt ...
    /etc/puppet/modules
    └── puppetlabs-stdlib (v4.12.0)
    Notice: Preparing to install into /etc/puppet/modules ...
    Notice: Downloading from https://forgeapi.puppetlabs.com ...
    Notice: Installing -- do not interrupt ...
    /etc/puppet/modules
    └── puppetlabs-inifile (v1.5.0)
    
    appears to do the right thing.

--- generate-rpki-mgmt-config.sh
+++ generate-rpki-mgmt-config.sh
@@ -444,7 +444,10 @@ if [ $group -eq 0 ]; then
    chgrp -R "$rm_group" "$rm_base"
 
    echo "* Installing required puppet modules"
-   puppet module install puppetlabs-stdlib puppetlabs-inifile
+
+   for module in puppetlabs-stdlib puppetlabs-inifile; do
+       puppet module install ${module}
+   done
 
 else
 
